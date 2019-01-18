36a15faadb493ee61d69fbdc5b097b922c53d0dd
Jake Brackney <brackney.jake@gmail.com>
2019-01-18 15:43:31 -0500

    more file set up for initial homepage render

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+
 app.get('/', (request, response) => {
-    response.send("Hello World")
+	response.send("Hello World")
 })
 
 app.listen(4000, () => {
-    console.log('You are listening on 4000')
+	console.log('You are listening on 4000')
 })
