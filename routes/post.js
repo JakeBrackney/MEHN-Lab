98c57348d0c239fea9066e74dff5d2355c01ed8b
Jake Brackney <brackney.jake@gmail.com>
2019-01-18 17:59:33 -0500

    added post route

---
+++ routes/post.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const postController = require('../controllers/post')
+
+router.post('/', postController.create)
+router.get('/new', postController.new)
+router.get('/:id', postController.show)
+router.put("/:id", postController.update)
+
+module.exports = router
