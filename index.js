7e3d505906c85033701f83110dc341aafb44a438
Jake Brackney <brackney.jake@gmail.com>
2019-01-18 15:50:01 -0500

    Installed HBS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const hbs = require("hbs");
 
 
-app.get('/', (request, response) => {
-	response.send("Hello World")
-})
+// app.get('/', (request, response) => {
+// 	response.send("Hello World")
+// })
+
+app.use(require("./routes/homepage.js"));
 
 app.listen(4000, () => {
 	console.log('You are listening on 4000')
--- routes/homepage.js
+++ routes/homepage.js
@@ -2,3 +2,6 @@ const express = require('express');
 const router = express.Router();
 const homepage = require('./controllers/homepage.js')
 
+router.get('/', homepage.index)
+
+module.exports = router;
