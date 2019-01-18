b76afc8ed47dc1e5a2932d626e3191738ce8a7a4
Rendon Elsesser <rendonelsesser@gmail.com>
2019-01-18 16:02:19 -0500

    adding routes and controller for homepage

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const app = express()
 const hbs = require("hbs");
-
+const bodyParser = require('body-parser')
+const app = express()
 
 // app.get('/', (request, response) => {
 // 	response.send("Hello World")
 // })
 
-app.use(require("./routes/homepage.js"));
+// app.use(require("./routes/homepage.js"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "hbs");
+app.use(require("./routes/index.js"));
 
 app.listen(4000, () => {
 	console.log('You are listening on 4000')
--- routes/application.js
+++ routes/application.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const homepage = require('./controllers/homepage.js')
+const homepage = require('../controllers/application')
 
 router.get('/', homepage.index)
 
