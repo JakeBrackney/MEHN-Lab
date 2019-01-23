14fb9a99bb7a48fb23d81381e9672e9b8932628a
Jake Brackney <brackney.jake@gmail.com>
2019-01-23 14:25:17 -0500

    Created 'New and Create' controllers for Post.js

--- controllers/post.js
+++ controllers/post.js
@@ -7,10 +7,18 @@ module.exports = {
     .populate("content")
 },
     new: (req, res) => {
-
+        res.render("post/new")
     },
     create: (req, res) => {
-
+        Post.create ({
+            title: req.body.post.text,
+            url: req.body.post.link
+        }).then(obj => {
+            req.user.post.push(obj)
+            req.user.save(err => {
+                res.redirect(`/post/${post._id}`)
+            })
+        })
     },
     delete: (req, res) => {}
 }
