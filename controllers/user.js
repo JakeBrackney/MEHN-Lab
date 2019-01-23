c0cb999ced24ea89874b91b9d2e7546070844601
Rendon Elsesser <rendonelsesser@gmail.com>
2019-01-23 17:34:32 -0500

    user/login works, login directs to home

--- controllers/user.js
+++ controllers/user.js
@@ -2,37 +2,76 @@
 const User = require("../models/User")
 const passport = require("passport");
 
-
-
 module.exports = {
-    index: (req,res) => {
-        User.find({})
-        .then(user => {
-            res.render('user/signup', { user })
+    show: (req, res) => {
+      User.findOne({ _id: req.params.id })
+        .populate({
+          path: "tweets",
+          options: { limit: 5, sort: { createdAt: -1 } }
         })
+        .then(user => {
+          res.render("user/show", { user });
+        });
     },
     login: (req, res) => {
-        res.render("user/login", { message: req.flash("loginMessage") });
-      },
-    show: (req, res) => {
-        User.findById(req.params.id)
-            .populate({
-                path: "post",
-                options: { limit: 5, sort: { createdAt: -1}}  
-            })
-            .then(user => {
-                res.render("user/signup", { user })
-            })
+      res.render("user/login", { message: req.flash("loginMessage") });
     },
-    new: (req, res) => {
-        res.render("user/signup", { user })
+    createLogin: (req, res) => {
+      const login = passport.authenticate("local-login", {
+        successRedirect: "/",
+        failureRedirect: "user/login",
+        failureFlash: true
+      });
+  
+      return login(req, res);
     },
-    create: (req, res) => {
-        User.create({
-            username: req.body.username,
-            password: req.body.password
-        }).then(user => {
-            res.redirect(`/user/${user._id}`)
-        })
+    signUp: (req, res) => {
+      res.render("user/signup", { message: req.flash("signupMessage") });
+    },
+    createSignUp: (req, res) => {
+      const signup = passport.authenticate("local-signup", {
+        successRedirect: "/",
+        failureRedirect: "user/signup",
+        failureFlash: true
+      });
+  
+      return signup(req, res);
+    },
+    logout: (req, res) => {
+      req.logout();
+      res.redirect("/");
     }
-}
+  };
+
+// module.exports = {
+//     index: (req,res) => {
+//         User.find({})
+//         .then(user => {
+//             res.render('user/signup', { user })
+//         })
+//     },
+//     login: (req, res) => {
+//         res.render("user/login", { message: req.flash("loginMessage") });
+//       },
+//     show: (req, res) => {
+//         User.findById(req.params.id)
+//             .populate({
+//                 path: "post",
+//                 options: { limit: 5, sort: { createdAt: -1}}  
+//             })
+//             .then(user => {
+//                 res.render("user/signup", { user })
+//             })
+//     },
+//     new: (req, res) => {
+//         res.render("user/signup", { user })
+//     },
+//     create: (req, res) => {
+//         User.create({
+//             username: req.body.username,
+//             password: req.body.password
+//         }).then(user => {
+//             res.redirect(`/user/${user._id}`)
+//         })
+//     }
+// }
--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,12 @@ const router = express.Router()
 const passport = require('passport')
 const userController = require("../controllers/user")
 
-
-router.post("/", userController.create)
-router.get('/login', userController.login)
-router.get("/new", userController.new)
-router.get("/:id", userController.show)
-// router.put("/:id", userController.update)
-
-
-
-// GET /
-// router.get('/', (req, res) => {
-//     res.render('index')
-// })
+router.get('/login', userController.login);
+router.post('/login', userController.createLogin);
+router.get('/sign-up', userController.signUp);
+router.post('/sign-up', userController.createSignUp);
+router.get('/logout', userController.logout);
+router.get('/:id', userController.show);
 
 
 router.post('/signup', (req, res) => {
@@ -26,45 +19,5 @@ router.post('/signup', (req, res) => {
     });
     return signupStrategy(req, res)
 })
-
-// // GET /signup
-// router.get('user/signup', (req, res) => {
-//     res.render('user/signup', {message: req.flash('signupMessage')})
-// })
-
-// // POST /signup
-// router.post('user/signup', (req, res, next) => {
-//     let signupStrategy = passport.authenticate('local-signup', {
-//         successRedirect: '/',
-//         failureRedirect: 'user/signup',
-//         failureFlash: true
-//     })
-
-//     return signupStrategy(req, res, next)
-// })
-
-// // GET /login
-// router.get('user/login', (req, res) => {
-//     res.render('user/login', {message: req.flash('loginMessage')})
-// })
-
-// // POST /login
-// router.post('user/login', (req, res, next) => {
-//     let loginStrategy = passport.authenticate('local-login', {
-//         successRedirect: '/',
-//         failureRedirect: 'user/login',
-//         failureFlash: true
-// })
-
-// return loginStrategy(req, res, next)
-// })
-
-// // GET /logout
-// router.get('/logout', (req, res) => {
-// })
-
-// // Restricted (cool people only!)
-// router.get('/secret', (req, res) => {
-// })
 
 module.exports = router
