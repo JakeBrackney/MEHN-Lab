3137fd75eebb8ee81a7e04a0a4237fc86f636be5
Jake Brackney <brackney.jake@gmail.com>
2019-01-24 11:45:33 -0500

    Successfully routing to New Post view

--- controllers/post.js
+++ controllers/post.js
@@ -1,26 +1,26 @@
-const Post = require('../models/post')
+const Post = require('../models/Post')
 // const Comment = require('../models/Comment')
 
 module.exports = {
-    show: (req, res) => {
-    Post.findOne({_id: req.params.id})
-    .populate("content")
-},
+//     show: (req, res) => {
+//     Post.findOne({_id: req.params.id})
+//     .populate("text")
+// },
     new: (req, res) => {
-        res.render("post/new")
-    },
-    create: (req, res) => {
-        Post.create ({
-            title: req.body.post.text,
-            url: req.body.post.link
-        }).then(obj => {
-            req.user.post.push(obj)
-            req.user.save(err => {
-                res.redirect(`/post/${post._id}`)
-            })
-        })
-    },
-    delete: (req, res) => {}
+        res.render("post/new") //
+    }
+    // create: (req, res) => { // POST request, 
+    //     Post.create ({
+    //         title: req.body.post.text,
+    //         url: req.body.post.link
+    //     }).then(obj => {
+    //         req.user.post.push(obj)
+    //         req.user.save(err => {
+    //             res.redirect(`/post/${post._id}`)
+    //         })
+    //     })
+    // },
+    // delete: (req, res) => {}
 }
 
 // new display the form
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router()
 
 router.use('/', require("./application.js"))
 router.use('/user', require('./user'))
+router.use('/post', require('./post'))
+
+//create postcontroller
 
 module.exports = router;
